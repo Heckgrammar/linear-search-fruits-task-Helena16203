3aca6ff33b868f2d72086729e3aa0f7d110c207b
Helena16203 <144434544+Helena16203@users.noreply.github.com>
2025-02-05 22:40:30 +0000

    Update Program.cs

--- LinearSearchFruitsTask/Program.cs
+++ LinearSearchFruitsTask/Program.cs
@@ -13,6 +13,25 @@
             
             // You must write your own linear search routine and not use any built-in fuctions available in C#
             // You should use meaningful variable names
+            
+             bool Found = false;
+             Console.WriteLine("What word would you like to find?");
+             string TargetFruit = Console.ReadLine();
+             for (int i = 0; i < fruits.Length; i++)
+             {
+                 if (fruits[i] == TargetFruit)
+                 {
+                     Found = true;
+                 }
+             }
+             if (Found == false)
+             {
+                 Console.WriteLine("Not found");
+             }
+             else if (Found == true)
+             {
+                 Console.WriteLine("Found");
+             }
         }
     }
 }
